f439ad9744096c8a8a15de829b9bd6eb431c289e
Gabor Dragos <gabordragos@gmail.com>
2020-04-04 13:22:08 +0200

    Eliminated stupid check.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,36 +2,34 @@
 
 import { register } from "register-service-worker";
 
-if (process.env.NODE_ENV === "production") {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        "App is being served from cache by a service worker.\n" +
-          "For more details, visit https://goo.gl/AFskqB"
-      );
-    },
-    registered() {
-      console.log("Service worker has been registered.");
-    },
-    cached() {
-      console.log("Content has been cached for offline use.");
-    },
-    updatefound() {
-      console.log("New content is downloading.");
-    },
-    updated() {
-      console.log("New content is available; please refresh.");
-    },
-    offline() {
-      console.log(
-        "No internet connection found. App is running in offline mode."
-      );
-    },
-    error(error) {
-      console.error("Error during service worker registration:", error);
-    }
-  });
-}
+register(`${process.env.BASE_URL}service-worker.js`, {
+  ready() {
+    console.log(
+      "App is being served from cache by a service worker.\n" +
+        "For more details, visit https://goo.gl/AFskqB"
+    );
+  },
+  registered() {
+    console.log("Service worker has been registered.");
+  },
+  cached() {
+    console.log("Content has been cached for offline use.");
+  },
+  updatefound() {
+    console.log("New content is downloading.");
+  },
+  updated() {
+    console.log("New content is available; please refresh.");
+  },
+  offline() {
+    console.log(
+      "No internet connection found. App is running in offline mode."
+    );
+  },
+  error(error) {
+    console.error("Error during service worker registration:", error);
+  }
+});
 
 // eslint-disable-next-line no-unused-vars
 async function checkSubscription() {
@@ -62,6 +60,7 @@ async function subscribeNotifications() {
       userVisibleOnly: false
     })
     .then(async function(sub) {
+      console.log("Got subscription: " + sub);
       return sub;
     })
     .catch(function(e) {
@@ -76,68 +75,71 @@ async function subscribeNotifications() {
   return subscription;
 }
 
-self.addEventListener("push", function(e) {
-  var body;
-  console.log("Got a push!");
-  if (e.data) {
-    body = e.data.text();
-  } else {
-    body = "Push message no payload";
-  }
+if ("Notification" in window && navigator.serviceWorker) {
+  // Display the UI to let the user toggle notifications
+  if (Notification.permission === "granted") {
+    /* do our magic */
+    self.addEventListener("notificationclose", function(e) {
+      var notification = e.notification;
+      var primaryKey = notification.data.primaryKey;
+      console.log("Closed notification: " + primaryKey);
+    });
+    console.log("Added Event Listener for Notification Close");
+    self.addEventListener("notificationclick", function(e) {
+      var notification = e.notification;
+      // eslint-disable-next-line no-unused-vars
+      var primaryKey = notification.data.primaryKey;
+      var action = e.action;
 
-  var options = {
-    body: body,
-    icon: "images/notification-flat.png",
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1
-    },
-    actions: [
-      {
-        action: "explore",
-        title: "Explore this new world",
-        icon: "images/checkmark.png"
-      },
-      {
-        action: "close",
-        title: "I don't want any of this",
-        icon: "images/xmark.png"
+      if (action === "close") {
+        notification.close();
+      } else {
+        // eslint-disable-next-line no-undef
+        clients.openWindow("http://www.google.com");
+        notification.close();
       }
-    ]
-  };
-  e.waitUntil(self.registration.showNotification("Push Notification", options));
-});
-
-if (process.env.NODE_ENV === "production") {
-  if ("Notification" in window && navigator.serviceWorker) {
-    // Display the UI to let the user toggle notifications
-    if (Notification.permission === "granted") {
-      /* do our magic */
-      self.addEventListener("notificationclose", function(e) {
-        var notification = e.notification;
-        var primaryKey = notification.data.primaryKey;
-        console.log("Closed notification: " + primaryKey);
-      });
-      self.addEventListener("notificationclick", function(e) {
-        var notification = e.notification;
-        // eslint-disable-next-line no-unused-vars
-        var primaryKey = notification.data.primaryKey;
-        var action = e.action;
-
-        if (action === "close") {
-          notification.close();
-        } else {
-          // eslint-disable-next-line no-undef
-          clients.openWindow("http://www.google.com");
-          notification.close();
-        }
-      });
-    } else if (Notification.permission === "blocked") {
-      /* the user has previously denied push. Can't reprompt. */
-    } else {
-      /* show a prompt to the user */
-    }
+    });
+    console.log("Added Event Listener for Notification Click");
+    self.addEventListener("push", function(e) {
+      var body;
+      console.log("Got a push!");
+      if (e.data) {
+        body = e.data.text();
+      } else {
+        body = "Push message no payload";
+      }
+      var options = {
+        body: body,
+        icon: "images/notification-flat.png",
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        actions: [
+          {
+            action: "explore",
+            title: "Explore this new world",
+            icon: "images/checkmark.png"
+          },
+          {
+            action: "close",
+            title: "I don't want any of this",
+            icon: "images/xmark.png"
+          }
+        ]
+      };
+      e.waitUntil(
+        self.registration.showNotification("Push Notification", options)
+      );
+    });
+    console.log("Added Event Listener for Push");
+  } else if (Notification.permission === "blocked") {
+    /* the user has previously denied push. Can't reprompt. */
+    console.log("Notification permission is blocked");
+  } else {
+    /* show a prompt to the user */
+    console.log("Something else");
   }
 }
 
